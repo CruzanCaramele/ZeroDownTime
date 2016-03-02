247dd42d175d938a4ae365e0211234a484e22576
CruzanCaramele <letters2hamza@gmail.com>
2016-03-02 19:03:37 +0800

    install apache

---
+++ ops/packer/puppet/modules/apache/manifests/package.pp
@@ -0,0 +1,13 @@
+# Class: apache::package
+#
+#
+class apache::package (
+	$package_name = $apache::params::package_name
+
+	) {
+	# resources
+	package { 'apache':
+		name   => $package_name,
+		ensure => installed,
+	}
+}
