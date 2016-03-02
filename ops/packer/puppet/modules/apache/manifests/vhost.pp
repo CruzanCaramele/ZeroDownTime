384d0ac0152fd2978136dfe1b0c8ef4d9158af80
CruzanCaramele <letters2hamza@gmail.com>
2016-03-02 21:01:29 +0800

    add application configuration

--- ops/packer/puppet/modules/apache/manifests/vhost.pp
+++ ops/packer/puppet/modules/apache/manifests/vhost.pp
@@ -1,40 +1,31 @@
 # Define: apache::vhost
-#rs:
+# Parameters:
 # arguments
 #
 define apache::vhost (
-	$port, $document_root, $servername, $vhost_name = '*', $vhost_dir, $app_dir
+	
+	$port, $document_root, $servername, $vhost_name = '*', $vhost_dir
 
 	) {
 	# puppet code
+
 	File {
 		mode => 0677,
 	}
 
-	vcsrepo {$app_dir
-		ensure   => latest,
-		provider => git,
-		source   => 'https://github.com/CruzanCaramele/Portfolio.git',
-		revision => master,
-		force    => true,
-		require  => Package['apache'],
-		notify   => Service['apache'],
-		before   => File['config_file'],
+	file { 'index':
+		path    => "${document_root}/index.html"
+		ensure  => file,
+		content => 'puppet:///modules/apache/index.html',
+		before  => File['config_file'],
+
 	}
 
 	file { 'config_file':
-		path    => "${vhost_dir}/000-default.conf",
+		path    => "${vhost_dir}/${servername}.conf"
 		ensure  => file,
 		content => template('apache/vhost.conf.erb'),
 		require => Package['apache'],
 		notify  => Service['apache'],
-	}
-
-	exec { 'enable-apachesite':
-		command      => 'sudo a2ensite 000-default.conf',
-		notify       => Service['apache'],
-		path         => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
-		require      => File['config_file'],
-		#refreshonly => true,
 	}
 }
