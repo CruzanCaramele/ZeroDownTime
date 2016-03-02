f203519ce101a88a3e66e9a4f80d741f83acc9af
CruzanCaramele <letters2hamza@gmail.com>
2016-03-02 19:04:19 +0800

    configure apache2 service

---
+++ ops/packer/puppet/modules/apache/manifests/service.pp
@@ -0,0 +1,18 @@
+# Class: apache::service
+#
+#
+class apache::service (
+	$service_name = $apache::params::service_name
+
+	) {
+	# resources
+	service { 'apache':
+		name        => $service_name,
+		enable      => true,
+		ensure      => running,
+		require     => Package['apache'],
+		#hasrestart => true,
+		#hasstatus  => true,
+		#require    => Class["config"],
+	}
+}
