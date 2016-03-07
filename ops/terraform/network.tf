27f46c0e702c3249ba8ccdbc63d3e95ff8f78640
CruzanCaramele <letters2hamza@gmail.com>
2016-03-07 17:22:15 +0800

    add route table association

--- ops/terraform/network.tf
+++ ops/terraform/network.tf
@@ -78,4 +78,16 @@ resource "aws_elb" "load_balancer" {
 		lb_port           = 80
 		lb_protocol       = "http"
 	}
+
+	health_check {
+		healthy_threshold 	= 2
+		unhealthy_threshold = 2
+		timeout 			= 3
+		target 				= "HTTP:80/"
+		interval 			= 30
+	}
+
+	lifecycle {
+		create_before_destroy = true
+	}
 }
