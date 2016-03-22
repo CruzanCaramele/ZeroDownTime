74db30889007d59357750c3f89aa1dbc63a69e6c
CruzanCaramele <letters2hamza@gmail.com>
2016-03-22 19:05:15 +0800

    add ssh_key module

--- ops/terraform/instances.tf
+++ ops/terraform/instances.tf
@@ -10,20 +10,30 @@ provider "aws" {
 #--------------------------------------------------------------
 # Artifact(s)
 #--------------------------------------------------------------
-resource "atlas_artifact" "zero" {
-	name    = "Panda/zero"
+resource "atlas_artifact" "ZeroImage" {
+	name    = "Panda/ZeroImage"
 	type    = "amazon.ami"
 	version = "latest"
+}
+
+#--------------------------------------------------------------
+# Module(s)
+#--------------------------------------------------------------
+module "ssh_keys" {
+	source = "./ssh_keys"
+	name   = "ZeroKey"
 }
 
 #--------------------------------------------------------------
 # Instance(s)
 #--------------------------------------------------------------
 resource "aws_instance" "zero-down-time" {
-	ami = "${atlas_artifact.zero.metadata_full.ami_id}"
-	instance_type = "t1.micro"
-	subnet_id = "${aws_subnet.public.id}"
-	vpc_security_group_ids = ["${aws_security_group.web.id}"]
+	ami                    = "${atlas_artifact.ZeroImage.metadata_full.ami_id}"
+	key_name               = "${module.ssh_keys.key_name}"
+	instance_type          = "t1.micro"
+	subnet_id              = "${aws_subnet.public.id}"
+	vpc_security_group_ids = ["${aws_security_group.default.id}, ${aws_security_group.web-ssh.id}"]
+	depends_on             = ["aws_internet_gateway.gateway"]
 
 	count = 2
 
