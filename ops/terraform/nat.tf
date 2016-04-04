6ee8b242acfe920aa165eeea5c0a05c662d39680
CruzanCaramele <letters2hamza@gmail.com>
2016-04-04 22:15:02 +0800

    correct syntanx for depends_on

--- ops/terraform/nat.tf
+++ ops/terraform/nat.tf
@@ -17,7 +17,7 @@ resource "aws_nat_gateway" "nat_gateway" {
 	count         = 1
 	allocation_id = "${aws_eip.nat_eip.id}"
 	subnet_id     = "${aws_subnet.public.id}"
-	depends_on    = "${aws_internet_gateway.gateway.id}"
+	depends_on    = ["aws_internet_gateway.gateway"]
 
 	lifecycle {
 		create_before_destroy = true
