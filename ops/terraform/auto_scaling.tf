9a9099e9e457231e002832f48766a998f829b682
CruzanCaramele <letters2hamza@gmail.com>
2016-04-06 19:48:54 +0800

    add load balancer to instances of the ASG

--- ops/terraform/auto_scaling.tf
+++ ops/terraform/auto_scaling.tf
@@ -8,8 +8,26 @@
 # Launch Configuration
 #--------------------------------------------------------------
 resource "aws_launch_configuration" "scale_config" {
-	name          = "web_servers_scale"
-	image_id      = "${atlas_artifact.ZeroImage.metadata_full.ami_id}"
-	instance_type = "t1.micro"
-	key_name      = "${module.ssh_keys.key_name}" 
+	name              = "web_servers_scale"
+	image_id          = "${atlas_artifact.ZeroImage.metadata_full.ami_id}"
+	instance_type     = "t1.micro"
+	key_name          = "${module.ssh_keys.key_name}"
+	enable_monitoring = true
+	security_groups   = ["${aws_security_group.web-ssh.id}"]
+
+	lifecycle {
+		create_before_destroy = true
+	}
+}
+
+#--------------------------------------------------------------
+# Auto Scaling Group
+#--------------------------------------------------------------
+resource "aws_auto_scaling_group" "scaling_group" {
+	name                = "scaling_group"
+	availability_zones  = ["us-east-1a"]
+	min_size            = 2
+	max_size	   	    = 5
+	vpc_zone_identifier = ["${aws_subnet.private_subnet.id}"]
+	load_balancers      = ["${aws_elb.ZeroBalancer.name}"]
 }
