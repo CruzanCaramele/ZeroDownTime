987d501af40d8f1a078bf0371e54732ec5683321
CruzanCaramele <letters2hamza@gmail.com>
2016-04-07 21:08:08 +0800

    create route53 record with elb as alias

--- ops/terraform/dnsrecord.tf
+++ ops/terraform/dnsrecord.tf
@@ -2,6 +2,28 @@
 # Route53 Zone
 #--------------------------------------------------------------
 resource "aws_route53_zone" "primary" {
-	name = "www.example.com"
+	name = "example.com"
+
+	lifecycle {
+		create_before_destroy = true
+	}
 }
 
+#--------------------------------------------------------------
+# Route53 Record
+#--------------------------------------------------------------
+resource "aws_route53_record" "www" {
+	zone_id = "{aws_route53_zone.primary.zone_id}"
+	name    = "example.com"
+	type    = "A"
+
+	alias {
+		name    			   = "${aws_elb.ZeroBalancer.dns_name}"
+		zone_id 			   = "${aws_elb.ZeroBalancer.zone_id}"
+		evaluate_target_health = true
+	}
+
+	lifecycle {
+		create_before_destroy = true
+	}
+}
