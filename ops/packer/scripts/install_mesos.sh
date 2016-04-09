f583598481a79599af142e1932e2a52494be4276
CruzanCaramele <letters2hamza@gmail.com>
2016-04-09 20:52:16 +0800

    download mesosphere's project key from ubuntu's keyserver,craft correct URL for ubuntu realese

--- ops/packer/scripts/install_mesos.sh
+++ ops/packer/scripts/install_mesos.sh
@@ -1,1 +1,10 @@
 #!usr/bin/env bash
+set -eux
+set -o pipefail
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
+DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
+CODENAME=$(lsb_release -cs)
+echo "deb http://repos.mesosphere.io/${DISTRO} ${CODENAME} main" | sudo tee /etc/apt/sources.list.d/mesosphere.list >/dev/null
+
+sudo apt-get -y update
