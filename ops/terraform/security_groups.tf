4fc39732699ea470154a2d2c5f1f57c01fd89ecd
CruzanCaramele <letters2hamza@gmail.com>
2016-04-10 19:10:33 +0800

    add consul security group

--- ops/terraform/security_groups.tf
+++ ops/terraform/security_groups.tf
@@ -1,5 +1,5 @@
 #--------------------------------------------------------------
-# Security group(s)
+# Security group(s) - Web
 #--------------------------------------------------------------
 resource "aws_security_group" "web-ssh" {
 	description = "allow access to the web and ssh"
@@ -49,5 +49,93 @@ resource "aws_security_group" "web-ssh" {
 
     lifecycle {
         create_before_destroy = true
+    }
+}
+
+#--------------------------------------------------------------
+# Security group - Consul
+#--------------------------------------------------------------
+resource "aws_security_group" "consul_security_group" {
+    description = "Security Group for consul"
+    vpc_id = "${aws_vpc.zero_vpc.id}"
+
+    // allows traffic from the SG itself for tcp
+    ingress {
+        from_port = 0
+        to_port = 65535
+        protocol = "tcp"
+        self = true
+    }
+
+    // allows traffic from the SG itself for udp
+    ingress {
+        from_port = 0
+        to_port = 65535
+        protocol = "udp"
+        self = true
+    }
+
+    // allow traffic for TCP 22 (SSH)
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["${var.source_cidr_block}"]
+    }
+
+    // allow traffic for TCP 8300 (Server RPC)
+    ingress {
+        from_port = 8300
+        to_port = 8300
+        protocol = "tcp"
+        cidr_blocks = ["${var.source_cidr_block}"]
+    }
+
+    // allow traffic for TCP 8301 (Serf LAN)
+    ingress {
+        from_port = 8301
+        to_port = 8301
+        protocol = "tcp"
+        cidr_blocks = ["${var.source_cidr_block}"]
+    }
+
+    // allow traffic for UDP 8301 (Serf LAN)
+    ingress {
+        from_port = 8301
+        to_port = 8301
+        protocol = "udp"
+        cidr_blocks = ["${var.source_cidr_block}"]
+    }
+
+    // allow traffic for TCP 8400 (Consul RPC)
+    ingress {
+        from_port = 8400
+        to_port = 8400
+        protocol = "tcp"
+        cidr_blocks = ["${var.source_cidr_block}"]
+    }
+
+    // allow traffic for TCP 8500 (Consul Web UI)
+    ingress {
+        from_port = 8500
+        to_port = 8500
+        protocol = "tcp"
+        cidr_blocks = ["${var.source_cidr_block}"]
+    }
+
+    // allow traffic for TCP 8600 (Consul DNS Interface)
+    ingress {
+        from_port = 8600
+        to_port = 8600
+        protocol = "tcp"
+        cidr_blocks = ["${var.source_cidr_block}"]
+    }
+
+    // allow traffic for UDP 8600 (Consul DNS Interface)
+    ingress {
+        from_port = 8600
+        to_port = 8600
+        protocol = "udp"
+        cidr_blocks = ["${var.source_cidr_block}"]
     }
 }
