aa74b90fb8dcf6d8e5e428aca1c7268180e0f2ee
CruzanCaramele <letters2hamza@gmail.com>
2016-04-14 22:07:37 +0800

    add subnet and parameter group names

--- ops/terraform/database.tf
+++ ops/terraform/database.tf
@@ -3,4 +3,9 @@ resource "aws_db_instance" "zero_database" {
 	engine			  = "postgres"
 	engine_version    = "9.4.4"
 	instance_class    = "db.t1.micro"
+	name			  = "zerodb"
+	username          = "myusername"
+	password          = "mypassword"
+	db_subnet_group_name = "my_database_subent_group"
+	parameter_group_name = "default.postgres9.4"
 }
