76502015bc157e901d5c0a15fe70ae6f5f86a857
CruzanCaramele <letters2hamza@gmail.com>
2016-04-14 22:10:08 +0800

    clean code

--- ops/terraform/database.tf
+++ ops/terraform/database.tf
@@ -1,11 +1,11 @@
 resource "aws_db_instance" "zero_database" {
-	allocated_storage = 10
-	engine			  = "postgres"
-	engine_version    = "9.4.4"
-	instance_class    = "db.t1.micro"
-	name			  = "zerodb"
-	username          = "myusername"
-	password          = "mypassword"
+	allocated_storage    = 10
+	engine			     = "postgres"
+	engine_version       = "9.4.4"
+	instance_class       = "db.t1.micro"
+	name			     = "zerodb"
+	username             = "myusername"
+	password             = "mypassword"
 	db_subnet_group_name = "my_database_subent_group"
 	parameter_group_name = "default.postgres9.4"
 }
