a59a6309dfc8a0904b9ed6af7eef452e125b9f6d
CruzanCaramele <letters2hamza@gmail.com>
2016-04-16 02:36:30 +0800

    create admin group policy

--- ops/terraform/user_management/users_aws.tf
+++ ops/terraform/user_management/users_aws.tf
@@ -12,3 +12,31 @@ resource "aws_iam_user" "sudoer" {
 resource "aws_iam_access_key" {
 	user = "${aws_iam_user.sudoer.name}"
 }
+
+#--------------------------------------------------------------
+# Sudo Group
+#--------------------------------------------------------------
+resource "aws_iam_group" "sudoers" {
+	name = "sudoers"
+	path = "/"
+}
+
+#--------------------------------------------------------------
+# Sudo Group Policy
+#--------------------------------------------------------------
+resource "aws_iam_group_policy" "sudoersPolicy" {
+	name   = "sudoersPolicy"
+	group  = "${aws_iam_group.sudoers.id}"
+	policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "*",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
