c3e22152fed748a4215a9e344a1515d6e10a6141
CruzanCaramele <letters2hamza@gmail.com>
2016-04-16 11:28:19 +0800

    add cloudtrail resource attributes

--- ops/terraform/user_management/cloudtrail.tf
+++ ops/terraform/user_management/cloudtrail.tf
@@ -3,7 +3,14 @@
 #--------------------------------------------------------------
 resource "aws_cloudtrail" "zeroTrail" {
 	name 		   				  = "zeroTrail"
-	s3_bucket_name 				  = "${aws_s3_bucket.zeroTrailBucket.id}"
-	s3_key_prefix  				  = "/prefix"
+	s3_bucket_name 				  = "${aws_s3_bucket.zero-trail-bucket.id}"
+	s3_key_prefix  				  = "/"
+	is_multi_region_trail         = true
 	include_global_service_events = false 
+	sns_topic_name				  = "zeroTrailNotifications"
+
+}
+
+resource "aws_s3_bucket" "zero-trail-bucket" {
+	bucket = 
 }
