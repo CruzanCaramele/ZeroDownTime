5542e0da2a12c146accc517ccb9a13783de2019c
CruzanCaramele <letters2hamza@gmail.com>
2016-04-16 11:32:53 +0800

    add s3 bucket policy for s3 cloudtrail bucket

--- ops/terraform/user_management/cloudtrail.tf
+++ ops/terraform/user_management/cloudtrail.tf
@@ -14,5 +14,34 @@ resource "aws_cloudtrail" "zeroTrail" {
 resource "aws_s3_bucket" "zero-trail-bucket" {
 	bucket        = "zero-trail-bucket"
 	force_destroy = true
-	policy        =
+	policy        = <<POLICY
+{
+	"Version": "2012-10-17",
+	"Statement": [
+		{
+			"Sid": "AWSCloudTrailAclCheck",
+			"Effect": "Allow",
+			"Principal": {
+				"Service": "cloudtrail.amazonaws.com"
+			},
+			"Action": "s3:GetBucketAcl",
+			"Resource": "arn:aws:s3:::zero-trail-bucket"
+		},
+		{
+			"Sid": "AWSCloudTrailWrite20150319",
+			"Effect": "Allow",
+			"Principal": {
+				"Service": "cloudtrail.amazonaws.com"
+			},
+			"Action": "s3:PutObject",
+			"Resource": "arn:aws:s3:::zero-trail-bucket/*",
+			"Condition": {
+				"StringEquals": {
+					"s3:x-amz-acl": "bucket-owner-full-control"
+				}
+			}
+		}
+	]
+}
+POLICY
 }
