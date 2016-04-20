b84899b88a430d59e3843be6c9ae38f4a5919f8a
CruzanCaramele <letters2hamza@gmail.com>
2016-04-20 22:05:43 +0800

    create zone for route53 for db instance

--- ops/terraform/dnsrecord.tf
+++ ops/terraform/dnsrecord.tf
@@ -1,29 +1,30 @@
-# #--------------------------------------------------------------
-# # Route53 Zone
-# #--------------------------------------------------------------
-# resource "aws_route53_zone" "primary" {
-# 	name = "example.com"
+#--------------------------------------------------------------
+# Route53 Zone for db-instance
+#--------------------------------------------------------------
+resource "aws_route53_zone" "db_zone" {
+	name   = "database.com"
+	vpc_id = "${aws_vpc.zero_vpc.id}" 
 
-# 	lifecycle {
-# 		create_before_destroy = true
-# 	}
-# }
+	lifecycle {
+		create_before_destroy = true
+	}
+}
 
-# #--------------------------------------------------------------
-# # Route53 Record
-# #--------------------------------------------------------------
-# resource "aws_route53_record" "www" {
-# 	zone_id = "{aws_route53_zone.primary.zone_id}"
-# 	name    = "example.com"
-# 	type    = "A"
+#--------------------------------------------------------------
+# Route53 Record for db-instance
+#--------------------------------------------------------------
+resource "aws_route53_record" "www" {
+	zone_id = "{aws_route53_zone.primary.zone_id}"
+	name    = "example.com"
+	type    = "A"
 
-# 	alias {
-# 		name    			   = "${aws_elb.ZeroBalancer.dns_name}"
-# 		zone_id 			   = "${aws_elb.ZeroBalancer.zone_id}"
-# 		evaluate_target_health = true
-# 	}
+	alias {
+		name    			   = "${aws_elb.ZeroBalancer.dns_name}"
+		zone_id 			   = "${aws_elb.ZeroBalancer.zone_id}"
+		evaluate_target_health = true
+	}
 
-# 	lifecycle {
-# 		create_before_destroy = true
-# 	}
-# }
+	lifecycle {
+		create_before_destroy = true
+	}
+}
