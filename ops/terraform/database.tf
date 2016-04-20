3cde9ca4c078a9819e5be93e53bec06876555806
CruzanCaramele <letters2hamza@gmail.com>
2016-04-20 21:35:07 +0800

    add db instance into multi availablity zone through multiple subnets

--- ops/terraform/database.tf
+++ ops/terraform/database.tf
@@ -1,4 +1,13 @@
 #--------------------------------------------------------------
+# DB-Subnet Group
+#--------------------------------------------------------------
+resource "aws_db_subnet_group" "zero_db_subnet_group" {
+	name        = "postgres"
+	description = "subnet group for database"
+	subnet_ids  = ["${aws_subnet.private_subnet.*.id}"]
+}
+
+#--------------------------------------------------------------
 # DB-Instance
 #--------------------------------------------------------------
 resource "aws_db_instance" "zero_database" {
@@ -15,14 +24,4 @@ resource "aws_db_instance" "zero_database" {
 	vpc_security_group_ids = ["${aws_security_group.web-ssh.id}", "${aws_security_group.consul_security_group.id}",
 							  "${aws_security_group.zookeeper_security_group.id}"]
 	db_subnet_group_name   = "${aws_db_subnet_group.zero_db_subnet_group.name}"
-}
-
-#--------------------------------------------------------------
-# DB-Subnet Group
-#--------------------------------------------------------------
-resource "aws_db_subnet_group" "zero_db_subnet_group" {
-	name        = "postgres"
-	description = "subnet group for database"
-	subnet_ids  = ["${aws_subnet.private_subnet.*.id}"]
-	#subnet_ids  = ["${split(",",aws_subnet.private_subnet.*.id)}"]
 }
