55a4ca2667bec5541b04c7d6bd895ccd39976671
CruzanCaramele <letters2hamza@gmail.com>
2016-04-20 22:16:32 +0800

    use aptitude install when apt-get fails

--- ops/packer/scripts/update.sh
+++ ops/packer/scripts/update.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -eux
 
-apt-get update
+apt-get -y update
 
 apt-get -y upgrade
 
+apt-get -y install aptitude
+
 # ensure the correct kernel headers are installed
-apt-get -y install linux-headers-$(uname -r)
+aptitude -y install linux-headers-$(uname -r)
 # fix broken dependencies
 apt-get -y -f install
 # update package index on boot
@@ -17,25 +19,15 @@ exec /usr/bin/apt-get update
 EOF
 
 # install curl to fix broken wget while retrieving content from secured sites
-apt-get -y install curl
+aptitude -y install curl
 
 # install general purpose packages
 # install rsync
-apt-get -y install rsync
+aptitude -y install rsync
 
 # install screen
-apt-get -y install screen
+aptitude -y install screen
 
 # install git
 echo "Installing git"
-apt-get -y install git
-
-# sshd config
-echo "UseDNS no" >> /etc/ssh/sshd_config
-echo "GSSAPIAuthentication no" >> /etc/ssh/sshd_config
-
-# sudoers config
-# Only add the secure path line if it is not already present - Debian 7
-# includes it by default.
-grep -q 'secure_path' /etc/sudoers || sed -i -e '/Defaults\s\+env_reset/a Defaults\tsecure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"' /etc/sudoers
-
+aptitude -y install git
