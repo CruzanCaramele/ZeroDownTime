e08645cf15af9bf875ae0617b93325ad5d128e78
CruzanCaramele <letters2hamza@gmail.com>
2016-04-22 20:22:28 +0800

    remove az temporarily

--- ops/terraform/instances.tf
+++ ops/terraform/instances.tf
@@ -31,12 +31,12 @@ resource "aws_instance" "zero-down-time" {
 	ami                    = "${atlas_artifact.ZeroImage.metadata_full.ami_id}"
 	key_name               = "${module.ssh_keys.key_name}"
 	instance_type          = "t1.micro"
-	availability_zone	   = "${element(split(",", var.azs), count.index)}"
+	#availability_zone	   = "${element(split(",", var.azs), count.index)}"
 	subnet_id              = "${element(aws_subnet.private_subnet.*.id, count.index)}"
 	monitoring			   = true
 	vpc_security_group_ids = ["${aws_security_group.web-ssh.id}" , "${aws_security_group.consul_security_group.id}",
 	  						  "${aws_security_group.zookeeper_security_group.id}"]
-	depends_on             = ["aws_internet_gateway.gateway"]
+	depends_on             = ["aws_internet_gateway.gateway", "aws_db_instance.zero_database"]
 
 	count = 2
 
