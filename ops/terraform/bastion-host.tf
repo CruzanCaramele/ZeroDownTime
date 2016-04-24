d8f4f96ecd7e96b183f763ad431e83c7d3ab9c05
CruzanCaramele <letters2hamza@gmail.com>
2016-04-25 02:17:40 +0800

    use ubuntu ami module look up from community

--- ops/terraform/bastion-host.tf
+++ ops/terraform/bastion-host.tf
@@ -1,6 +1,16 @@
+module "ami" {
+	source = "github.com/terraform-community-modules/tf_aws_ubuntu_ami"
+	region = "eu-central-1"
+	distribution = "trusty"
+	architecture = "amd64"
+	virttype = "hvm"
+	storagetype = "instance-store"
+}
+
 #--------------------------------------------------------------
-# elb
+# bastion - host
 #--------------------------------------------------------------
 resource "aws_instance" "bastion_host" {
-	
+	instance_type = "t1.micro"
+	ami           = "${module.ami.ami_id}"
 }
